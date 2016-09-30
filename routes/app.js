d3373e48e3f54db2d4698583b026a320fe836366
Blak3Nick <blaken2021@gmail.com>
2016-09-30 15:30:39 -0400

    added new views and a new route

--- routes/app.js
+++ routes/app.js
@@ -4,5 +4,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('index');
 });
+router.get('/node', function(req, res, next) {
+    res.render('node');
+});
 
 module.exports = router;
