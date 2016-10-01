262e92bf405cb9c527202a68d6960e8dc03b4207
Blak3Nick <blaken2021@gmail.com>
2016-10-01 10:19:19 -0400

    working with routes and views in express and node

--- routes/app.js
+++ routes/app.js
@@ -4,9 +4,7 @@ var router = express.Router();
 router.get('/:userTitle?', function(req, res, next) {
     res.render('node', { title: req.params.userTitle ? req.params.userTitle : ''});
 });
-// router.get('/node:userTitle?', function(req, res, next) {
-//     res.render('node', { title: req.params.userTitle ? req.params.userTitle : ''});
-// });
+
 router.post('/', function(req, res, next) {
     var title = req.body.title;
     res.redirect('/' + title);
