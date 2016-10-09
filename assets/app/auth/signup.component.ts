211763786428c095920bca51312ffbdad913ab13
Blak3Nick <blaken2021@gmail.com>
2016-10-08 20:02:59 -0400

    Setting up login

--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -1,30 +1,51 @@
-import {Component} from "angular2/core";
+import {Component, OnInit} from "angular2/core";
+import {FormBuilder, ControlGroup, Validators} from "angular2/common";
+
+
 @Component({
     selector: 'my-signup',
     template: `
     <section class="col-md-8 col-md-offset-2">
-    <form action="">
+    <form [ngFormModel]="myForm" (ngSubmit)="onSubmit()">
         <div class="form-group">
         <label for="firstName">First Name</label>
-        <input type="text" id="firstName" class="form-control">
+        <input [ngFormControl]="myForm.find('firstName')" type="text" id="firstName" class="form-control">
         </div>    
         <div class="form-group">
         <label for="lastName">Last Name</label>
-        <input type="text" id="lastName" class="form-control">
+        <input [ngFormControl]="myForm.find('lastName')" type="text" id="lastName" class="form-control">
         </div>
         <div class="form-group">
         <label for="email">Mail</label>
-        <input type="email" id="email" class="form-control">
+        <input [ngFormControl]="myForm.find('email')" type="email" id="email" class="form-control">
         </div>
         <div class="form-group">
         <label for="password">Password</label>
-        <input type="password" id="password" class="form-control">
+        <input [ngFormControl]="myForm.find('password')" type="password" id="password" class="form-control">
         </div>   
-        <button type="submit" class="btn btn-primary">Sign Up</button>
+        <button type="submit" class="btn btn-primary" [disabled]="!myForm.valid">Sign Up</button>
     </form>
 </section>`
 })
 
-export class SignupCOmponent {
+export class SignupComponent implements OnInit{
+    myForm: ControlGroup;
 
+    constructor(private _fb: FormBuilder) {
+
+    }
+
+    onSubmit(){
+       console.log(this.myForm.value);
+    }
+
+    ngOnInit() {
+        this.myForm = this._fb.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['', Validators.required],
+            password: ['', Validators.required]
+
+        });
+    }
 }
