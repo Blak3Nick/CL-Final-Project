943071979f62d16a8f24b8ec4ecc5e8ec458210c
Blak3Nick <blaken2021@gmail.com>
2016-10-21 12:14:50 -0400

    Setting up users

--- assets/app/auth/auth.service.ts
+++ assets/app/auth/auth.service.ts
@@ -27,4 +27,10 @@ export class AuthService {
             .map(response => response.json())
             .catch(error => Observable.throw(error.json()));
     }
+    logout() {
+        localStorage.clear();
+    }
+    isLoggedIn() {
+        return localStorage.getItem('token') !== null;
+    }
 }
